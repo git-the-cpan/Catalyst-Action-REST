66ef00257bd145466880546dd50ab2d9b2f77735
Hans Dieter Pearcey <hdp@pobox.com>
2009-03-28 16:19:10 +0000

    Import of HDP/Catalyst-Action-REST-0.71.tar.gz from CPAN.
    gitpan-cpan-distribution: Catalyst-Action-REST
    gitpan-cpan-version:      0.71
    gitpan-cpan-path:         HDP/Catalyst-Action-REST-0.71.tar.gz
    gitpan-cpan-author:       HDP
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Controller/REST.pm
+++ lib/Catalyst/Controller/REST.pm
@@ -1,6 +1,6 @@
 package Catalyst::Controller::REST;
 
-our $VERSION = '0.70';
+our $VERSION = '0.71';
 
 =head1 NAME
 
@@ -217,7 +217,6 @@ use Params::Validate qw(SCALAR OBJECT);
 __PACKAGE__->mk_accessors(qw(serialize));
 
 __PACKAGE__->config(
-    'default_view' => 'REST',
     'stash_key' => 'rest',
     'map'       => {
         'text/html'          => 'YAML::HTML',
@@ -236,6 +235,8 @@ __PACKAGE__->config(
 );
 
 sub begin : ActionClass('Deserialize') { }
+
+sub end : ActionClass('Serialize') { }
 
 =item status_ok
 
